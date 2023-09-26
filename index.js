2bdecd551a588e53e2459d3c22d8935936bbf2bc
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-09-26 17:23:38 -0300

    Refatorando o codigo gerado pelo  react-native init

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, DefaultTheme } from "react-native-paper";
+
+const theme = {
+    ...DefaultTheme,
+    dark:true,
+    colors:{
+        ...DefaultTheme,
+        primary: '#eee' 
+    }
+}
 
 export default function Main(){
     return(
-        <PaperProvider>
+        <PaperProvider theme={theme}>
             <App />
         </PaperProvider>
     )
