4cbb5091c9278a5efc04e051543d04368ab22757
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-11-09 15:50:40 -0300

    Foi criado o painel de navegacão inferior

--- app/ui/views/BottomNav.js
+++ app/ui/views/BottomNav.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import {BottomNavigation} from 'react-native-paper';
 import ProductView from './ProductView';
 import CartView from './CartView';
+import {CartContext} from '../providers/CartProvider'
 
 
 
 
 export default function BottomNav(props){
+
+    const [{products}] = useContext(CartContext);
     
     const [index, setIndex] = useState(0);
 
@@ -27,12 +30,20 @@ export default function BottomNav(props){
     ]);
 
     function renderScene({route, jumpTo}){
-        switch (route.key) {
-            case 'products':
-                return <ProductView jumpTo={jumpTo} />;
-            case 'cart':
-                return <CartView  jumpTo={jumpTo} />;  
+        
+        if (route.key !== routes[index].key) {
+            return null;
         }
+
+        const NewComponent = route.component;
+        return <NewComponent jumpTo = {jumpTo} />;
+    }
+
+    function handleBadge({route}){
+        if (route.key === 'cart' && products.length > 0 ) {
+            return products.length;
+        }
+        return false;
     }
 
     return(
@@ -43,6 +54,7 @@ export default function BottomNav(props){
         }}        
         renderScene={renderScene}
         onIndexChange={setIndex}
+        getBadge={handleBadge}
         />
     )
 }
